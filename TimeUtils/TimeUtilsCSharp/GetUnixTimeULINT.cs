92c97ed84a31667f54bf739bc1b1ff40e130a242
Ykrll <kirillarmak7@gmail.com>
2026-06-28 15:43:49 +0300

    inital commit

---
+++ TimeUtils/TimeUtilsCSharp/GetUnixTimeULINT.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Iec61131Lib;
+using Eclr;
+
+namespace TimeUtils
+{
+    [Native]
+    [Function]
+    public static class GetUnixTimeULINT
+    {
+        [Execution]
+        public static ulong __Process()
+        {
+            return 0;
+        }
+    }
+}
+
+
+// using System;
+// using System.Iec61131Lib;
+// using System.Runtime.InteropServices;
+// using Eclr;
+// using Iec61131.Engineering.Prototypes.Types;
+// using Iec61131.Engineering.Prototypes.Variables;
+// using Iec61131.Engineering.Prototypes.Common;
+// using Iec61131.Engineering.Prototypes.Methods;
+// using Iec61131.Engineering.Prototypes.Pragmas;
+
+// namespace TimeUtils
+// {
+//     [Native]
+//     [Function]
+//     public static class GetUnixTimeULINT
+//     {
+//         [Execution]
+//         public static ulong __Process(
+//             [Input] short IN1,
+//             [Input, DataType("WORD")] ushort IN2)
+//         {
+//             // No implementation in C# part; implement in native method
+//             // Just return something to avoid errors from the C# compiler.
+//             return 0;
+//         }
+//     }
+// }
